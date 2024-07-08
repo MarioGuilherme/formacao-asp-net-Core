baf980c8ccb1012337dd30ada1af388e25335b46
MarioGuilherme <marioguilhermedev@gmail.com>
2024-07-08 19:41:00 -0300

    style: identação padronizada

--- DevFreela.Application/Validators/CreateCommentCommandValidator.cs
+++ DevFreela.Application/Validators/CreateCommentCommandValidator.cs
@@ -6,7 +6,7 @@ namespace DevFreela.Application.Validators;
 public class CreateCommentCommandValidator : AbstractValidator<CreateCommentCommand> {
     public CreateCommentCommandValidator() {
         this.RuleFor(p => p.Content)
-                .MaximumLength(255)
-                .WithMessage("Tamanho máximo de Texto de Comentário é de 255 caracteres.");
+            .MaximumLength(255)
+            .WithMessage("Tamanho máximo de Texto de Comentário é de 255 caracteres.");
     }
 }
