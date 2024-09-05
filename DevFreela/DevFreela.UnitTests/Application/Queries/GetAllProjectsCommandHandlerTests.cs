b893a0c2524f72ea46d26eece98554d2692580f5
MarioGuilherme <marioguilhermedev@gmail.com>
2024-09-04 23:40:00 -0300

    test: correção do teste da busca de projetos

--- DevFreela/DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
+++ DevFreela/DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Application.Queries.GetAllProjects;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
+using DevFreela.Core.Models;
 using DevFreela.Core.Repositories;
 using Moq;
 
@@ -10,26 +11,28 @@ public class GetAllProjectsCommandHandlerTests {
     [Fact]
     public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels() {
         // Arrange
-        List<Project> projects = [
-            new("Nome de Teste 1", "Descrição de Teste 1", 1, 2, 10000),
-            new("Nome de Teste 2", "Descrição de Teste 2", 1, 2, 20000),
-            new("Nome de Teste 3", "Descrição de Teste 3", 1, 2, 30000)
-        ];
+        PaginationResult<Project> projects = new() {
+            Data = [
+                new("Nome de Teste 1", "Descrição de Teste 1", 1, 2, 10000),
+                new("Nome de Teste 2", "Descrição de Teste 2", 1, 2, 20000),
+                new("Nome de Teste 3", "Descrição de Teste 3", 1, 2, 30000)
+            ]
+        };
 
         Mock<IProjectRepository> projectRepositoryMock = new();
-        projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+        projectRepositoryMock.Setup(pr => pr.GetAllAsync(It.IsAny<string>(), It.IsAny<int>()).Result).Returns(projects);
 
-        GetAllProjectsQuery getAllProjectsQuery = new(string.Empty);
+        GetAllProjectsQuery getAllProjectsQuery = new() { Query = string.Empty, Page = 1 };
         GetAllProjectsQueryHandler getAllProjectsQueryHandler = new(projectRepositoryMock.Object);
 
         // Act
-        List<ProjectViewModel> projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new());
+        PaginationResult<ProjectViewModel> paginationProjectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new());
 
         // Assert
-        Assert.NotNull(projectViewModelList);
-        Assert.NotEmpty(projectViewModelList);
-        Assert.Equal(projects.Count, projectViewModelList.Count);
+        Assert.NotNull(paginationProjectViewModelList);
+        Assert.NotEmpty(paginationProjectViewModelList.Data);
+        Assert.Equal(projects.Data.Count, paginationProjectViewModelList.Data.Count);
 
-        projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+        projectRepositoryMock.Verify(pr => pr.GetAllAsync(It.IsAny<string>(), It.IsAny<int>()).Result, Times.Once);
     }
 }
